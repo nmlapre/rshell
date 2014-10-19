e1e8df558da738b5539ab4f0432d4a418b8768e6
Nathan LaPre <nlapr001@ucr.edu>
2014-10-19 16:23:47 -0700

    added the while loop to main and some preliminary function definitions

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -9,13 +9,27 @@
 using namespace std;
 using namespace boost;
 
+
+//will prompt the user for their input and get the input string
+string user_prompt();
+
+//tokenize the string using boost functions
+list<string> tokenize(string);
+
+
 int main(int argc, char** argv)
 {
+	while(1) {
+		tokenize(user_prompt());
+		//execvp that list son
+	}
 	/*
 	for (unsigned i = 0; i < argc; ++i) {
 		cout << "argv[" << i << "]: " << argv[i] << endl;
 	}
 	*/
+
+	
 	int r = execvp(argv[1], argv+1);
 	if (r == -1) {
 		perror("execvp");
@@ -32,4 +46,21 @@ int main(int argc, char** argv)
 		cout << t << endl;
 	}
 	*/
+}
+
+string user_prompt() {
+	cout << "~$ ";
+	string s;
+	cin >> s;
+	return s;
+}
+
+list<string> tokenize(string user_input) {
+	list<string> tokenList;
+	split(tokenList, user_input, is_any_of(" "), token_compress_on);
+	BOOST_FOREACH(string t, tokenList)
+	{
+		cout << t << endl;
+	}
+	return tokenList;
 }
