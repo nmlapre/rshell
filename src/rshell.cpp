f282ba25576bd2c8f8e1f2cedf904004765a9501
Nathan LaPre <nlapr001@ucr.edu>
2014-10-19 21:40:15 -0700

    added some more function definitions, touched up main structure

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -9,25 +9,27 @@
 using namespace std;
 using namespace boost;
 
-
 //will prompt the user for their input and get the input string
 string user_prompt();
 
 //tokenize the string using boost functions
 list<string> tokenize(string);
 
+//make tokens a character array instead of a list of strings
+char** to_char_array(list<string>);
 
-int main(int argc, char** argv)
+int main()
 {
 	while(1) {
-		tokenize(user_prompt());
-		//execvp that list son
+		list<string> tokens = tokenize(user_prompt());
+		char** argv = to_char_array(tokens);
+		execvp(argc
 	}
 	/*
 	for (unsigned i = 0; i < argc; ++i) {
 		cout << "argv[" << i << "]: " << argv[i] << endl;
 	}
-	*/
+	
 
 	
 	int r = execvp(argv[1], argv+1);
@@ -36,7 +38,7 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 
-	/*
+	
 	string text = "ls -a";
 
 	list<string> tokenList;
@@ -64,3 +66,15 @@ list<string> tokenize(string user_input) {
 	}
 	return tokenList;
 }
+
+char** to_char_array(list<string> tokens) {
+	int count = 0;
+	char **progArgs = new char*[tokens.size()];
+	for(list<string>::iterator t=tokens.begin(); t!=tokens.end(); t++)
+	{
+		progArgs[count] = strdup(t->c_str());
+       		count++;
+   	}
+	return progArgs;
+}
+
