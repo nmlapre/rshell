71aeaaaec6c8442b10cab30ef9b65dac115b3f9f
Nathan LaPre <nlapr001@ucr.edu>
2014-10-20 23:28:33 -0700

    changed the way exec and fork work

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -5,6 +5,9 @@
 #include <list>
 #include <boost/foreach.hpp>
 #include <boost/algorithm/string.hpp>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstdlib>
 
 using namespace std;
 using namespace boost;
@@ -16,60 +19,57 @@ string user_prompt();
 list<string> tokenize(string);
 
 //make tokens a character array instead of a list of strings
+//so that it can be passed safely to execvp
 char** to_char_array(list<string>);
 
 int main()
 {
 	while(1) {
+		string input = user_prompt();
 		list<string> tokens = tokenize(user_prompt());
 		char** argv = to_char_array(tokens);
-		execvp(argc
+		int pid = fork();
+		if (pid == 0) {
+			int r = execvp(argv[0], argv);
+			if (r == -1) {
+				//cout << "error!" << endl;
+				perror("execvp");
+				exit(EXIT_FAILURE);
+			}
+		exit(pid); //kill the child process when it is finished
+		} else {
+			if ( wait(0) == -1 ) {
+				perror("wait");
+				exit(EXIT_FAILURE);
+		}	}
 	}
-	/*
-	for (unsigned i = 0; i < argc; ++i) {
-		cout << "argv[" << i << "]: " << argv[i] << endl;
-	}
-	
-
-	
-	int r = execvp(argv[1], argv+1);
-	if (r == -1) {
-		perror("execvp");
-		exit(1);
-	}
-
-	
-	string text = "ls -a";
-
-	list<string> tokenList;
-	split(tokenList, text, is_any_of(" "), token_compress_on);
-	BOOST_FOREACH(string t, tokenList)
-	{
-		cout << t << endl;
-	}
-	*/
 }
 
 string user_prompt() {
 	cout << "~$ ";
 	string s;
-	cin >> s;
+	getline(cin, s);
 	return s;
 }
 
 list<string> tokenize(string user_input) {
 	list<string> tokenList;
 	split(tokenList, user_input, is_any_of(" "), token_compress_on);
-	BOOST_FOREACH(string t, tokenList)
-	{
-		cout << t << endl;
-	}
+	int count = 0;
+	//for (list<string>::const_iterator i = tokenList.begin(); i != tokenList.end(); ++i) {
+	//	cout << count << ": " <<  *i << endl;
+	//	count++;
+	//}
+	
 	return tokenList;
 }
 
 char** to_char_array(list<string> tokens) {
 	int count = 0;
 	char **progArgs = new char*[tokens.size()];
+	//for (list<string>::const_iterator i = tokens.begin(); i != tokens.end(); ++i) {
+	//	cout << *i << endl;
+	//}
 	for(list<string>::iterator t=tokens.begin(); t!=tokens.end(); t++)
 	{
 		progArgs[count] = strdup(t->c_str());
